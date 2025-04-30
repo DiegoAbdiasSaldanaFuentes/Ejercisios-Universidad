a015cf4dce84fb5dec11fa29ea539b647f70bdd9
DERPVTRV <derpvtrvtuber@gmail.com>
2025-04-30 14:52:38 -0400

    sql

---
+++ Segundo_año/SQL Server Management Studio/SQLQuery1.sql
@@ -0,0 +1,19 @@
+-- Esta es mi primera base de dato
+-- by Diego Saldaña
+
+CREATE DATABASE MinuevaBD; -- Crea la base de datos
+
+USE MinuevaBD; -- Activa la DB para trabajar con ella
+
+CREATE TABLE Usuarios(
+	ID INT PRIMARY KEY IDENTITY,
+	Nombre VARCHAR(50),
+	Correo VARCHAR(100)
+);
+
+--Insersion de datos
+INSERT INTO Usuarios(Nombre,Correo)
+	VALUES('Diego','diego.saldana@alumnos.ucm.cl');
+
+--Consuta de datos
+SELECT *FROM Usuarios;
