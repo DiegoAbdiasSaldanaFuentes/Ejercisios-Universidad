915891859c33a61ed1f4b35e2cd08e08cdd817f9
Diego Saldaña <saldanafuentesabdiasdiego@gmail.com>
2025-05-10 17:14:51 -0400

    powershell

---
+++ Segundo_año/PowerShell/probando_powershell.ps1
@@ -0,0 +1,22 @@
+﻿# Probando PowerShell
+# By Diego Saldaña
+# Fecha: 10/05/2025, Feliz dia de las madres
+
+$num_enteros=123 #Definimos una variable
+$num_enteros #Llamamos esa variable
+
+$caracteres= "hola mundo" #definimos una variable str
+$caracteres #llamamos esa funcion
+
+<#Comentario largo
+y de varias secciones#>
+
+#Bucle while
+$i=0 #creamos el contador iniciando en 0
+while($i -lt 5){ #creamos el bucle que recorra 4 veces
+    $i #imprimos el contador
+ #incrementamos el valor del contador
+ $i++
+}
+
+Write-Output "Hola con Write-out"
